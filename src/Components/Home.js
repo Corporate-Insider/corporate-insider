efa83e822a90387ea254b1331d9719010844b624
alishalawani <alishalawani@gmail.com>
2020-09-08 02:29:32 -0500

    made the images and company names link to an insight page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,33 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import {Image, Card, Container} from 'react-bootstrap';
-import './Home.css'
-const proxyurl = 'https://cors-anywhere.herokuapp.com/';
-const url = `http://localhost:8000/companies/`;
-function Home(props) {
-    // let [companies, setCompanies] = useState([]);
-    // useEffect(()=>{
-    //     fetch(url)
-	// 				.then((res) => {
-	// 					return res.json();
-	// 				})
-	// 				.then((res) => {
-    //                     console.log(res);
-    //                     setCompanies(companies = res);
-	// 				});
-    // }, [])
-    // let cards = companies.map((company, index)=>{
-    //     return (
-	// 				<Card key={index} className='mainCard'>
-	// 					<Card.Body className='card'>
-	// 						<Image src={company.logo} className='companyLogo' />
-	// 						<h5 className='companyName'>{company.name}</h5>
-	// 					</Card.Body>
-	// 				</Card>
-	// 			);
-    // })
+import './Home.css';
+import { Link } from 'react-router-dom';
+
+
+function Home({companies}) {
+  
+    let cards = companies.map((company, index)=>{
+        return (
+					<Card key={index} className='mainCard'>
+						<Card.Body className='card'>
+							<Link to={`insight/${company.name}`}>
+								<Image src={company.logo} className='companyLogo' />
+							</Link>
+							<Link to={`insight/${company.name}`} className='companyName'>
+								<h5>{company.name}</h5>
+							</Link>
+						</Card.Body>
+					</Card>
+				);
+    })
     return (
 			<div>
-				{/* {cards} */}
+				{cards}
 			</div>
 		);
 }
