6001a97f6e85196187b32df88669388db924f477
alishalawani <alishalawani@gmail.com>
2020-09-07 20:58:13 -0500

    made header dynamic

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,34 +1,13 @@
 import React from 'react';
 import './Header.css';
-import {Nav, Button, Navbar} from 'react-bootstrap';
+import { Nav, Button, Navbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-function Header(props) {
-    const loggedOutNav = (
-			<Navbar className='nav' collapseOnSelect variant='light' expand='md'>
-				<LinkContainer to='/'>
-					<Navbar.Brand>
-						<h1 className='logo'>Corporate Insider</h1>
-					</Navbar.Brand>
-				</LinkContainer>
-				<Navbar.Toggle aria-controls='basic-navbar-nav' />
-				<Navbar.Collapse id='basic-navbar-nav'>
-					<Nav>
-						<LinkContainer to='/'>
-							<Nav.Link>Companies</Nav.Link>
-						</LinkContainer>
+function Header({loggedIn, setLoggedIn}) {
+    const handleLogOut = (event) => {
+			event.preventDefault();
+		};
 
-						<LinkContainer to='/about'>
-							<Nav.Link>About</Nav.Link>
-						</LinkContainer>
-						<Button>Login</Button>
-						<Button>SignUp</Button>
-					</Nav>
-				</Navbar.Collapse>
-			</Navbar>
-		);
-
-
-	return (
+	const loggedOutNav = (
 		<Navbar className='nav' collapseOnSelect variant='light' expand='md'>
 			<LinkContainer to='/'>
 				<Navbar.Brand>
@@ -41,14 +20,48 @@ function Header(props) {
 					<LinkContainer to='/'>
 						<Nav.Link>Companies</Nav.Link>
 					</LinkContainer>
-                    
+
 					<LinkContainer to='/about'>
 						<Nav.Link>About</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to='/login' className='button'>
+						<Button>Login</Button>
+					</LinkContainer>
+
+					<LinkContainer to='/signup' className='button'>
+						<Button>SignUp</Button>
 					</LinkContainer>
 				</Nav>
 			</Navbar.Collapse>
 		</Navbar>
 	);
+	const loggedInNav = (
+		<Navbar className='nav' collapseOnSelect variant='light' expand='md'>
+			<LinkContainer to='/'>
+				<Navbar.Brand>
+					<h1 className='logo'>Corporate Insider</h1>
+				</Navbar.Brand>
+			</LinkContainer>
+			<Navbar.Toggle aria-controls='basic-navbar-nav' />
+			<Navbar.Collapse id='basic-navbar-nav'>
+				<Nav>
+					<LinkContainer to='/'>
+						<Nav.Link>Companies</Nav.Link>
+					</LinkContainer>
+
+					<LinkContainer to='/about'>
+						<Nav.Link>About</Nav.Link>
+					</LinkContainer>
+					<Button onClick={handleLogOut}>Logout</Button>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
+
+	
+
+	return loggedIn ? loggedInNav : loggedOutNav;
+
 }
 
 export default Header;
