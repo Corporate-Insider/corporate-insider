5c2d5ea9de4e25d4ced2c1506ff0683838e19d2f
alishalawani <alishalawani@gmail.com>
2020-09-09 22:37:31 -0500

    modified siggnup page

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
 
 const createUrl = `http://localhost:8000/accounts/create_user/`;
 function SignUp({setUsername}) {
@@ -7,64 +8,102 @@ function SignUp({setUsername}) {
         email: '',
         password: '',
     })
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [valid, setValid] = useState(true);
+   const handleSubmit = (event) => {
+			event.preventDefault();
+			if (info.password.length >= 6 && confirmPassword.length >= 6) {
+				if (info.password === confirmPassword) {
+					setValid(true);
+					fetch(createUrl, {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+						body: JSON.stringify(info),
+					})
+						.then((res) => res.json())
+						.then((res) => {
+							localStorage.setItem('token', res.token);
+                            localStorage.setItem('user', res.name);
+                            localStorage.setItem('userId', res.id);
+							setUsername(res.name);
+							console.log(res);
+							window.location.replace('/');
+						});
+				}
+			} else {
+				setValid(false);
+			}
+        };
+        const handleCancel = (event) => {
+					event.preventDefault();
+					setInfo({
+						name: '',
+						email: '',
+						password: '',
+					});
+					setConfirmPassword('');
+				};
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
 
-        fetch(createUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(info)
-        }).then((res)=> res.json()).then((res)=>{
-            localStorage.setItem('token', res.token)
-            localStorage.setItem('user', res.user.name);
-            setUsername(res.name)
-            
-            window.location.replace('/');
-        })
-    };
+const handleChange = (event) => {
+	let id = event.target.id;
+	let value = event.target.value;
+	if (id === 'email') {
+		setInfo({ name: info.name, email: value, password: info.password });
+	} else if (id === 'password') {
+		setInfo({ name: info.name, email: info.email, password: value });
+	} else if (id === 'username') {
+		setInfo({ name: value, email: info.email, password: info.password });
+	} else if (id === 'confirm') {
+		setConfirmPassword(value);
+	}
+};
 
-	const handleChange = (event) => {
-        let targetName = event.target.name;
-        let value = event.target.value;
-		if (targetName === 'email') {
-			setInfo({ name: info.name, email: value, password: info.password });
-		} else if (targetName === 'password') {
-			setInfo({ name: info.name, email: info.email , password: value });
-		}else if(targetName === 'username'){
-            setInfo({ name: value, email: info.email, password: info.password });
-        }
-	};
 
     return (
-			<form onSubmit={handleSubmit}>
-				<h4>Sign Up</h4>
-				<label htmlFor='username'>Username</label>
-				<input
-					type='text'
-					name='username'
-					value={info.name || ''} //because it displayed an uncontrolled error
-					onChange={handleChange}
-				/>
-				<label htmlFor='username'>Email</label>
-				<input
-					type='email'
-					name='email'
-					value={info.email || ''}
-					onChange={handleChange}
-				/>
+			<Container>
+				{!valid && <p>Your password is incorrect!</p>}
+				<form onSubmit={handleSubmit}>
+					<h4>Sign Up</h4>
+					<label htmlFor='username'>Username</label>
+					<input
+						type='text'
+						id='username'
+						value={info.name || ''} //because it displayed an uncontrolled error
+						onChange={handleChange}
+					/>
+					<label htmlFor='username'>Email</label>
+					<input
+						type='email'
+						id='email'
+						value={info.email || ''}
+						onChange={handleChange}
+					/>
 
-				<label htmlFor='password'>Password</label>
-				<input
-					type='password'
-					name='password'
-					value={info.password || ''}
-					onChange={handleChange}
-				/>
-				<input type='submit' />
-			</form>
+					<label htmlFor='password'>Password</label>
+					<input
+						type='password'
+						id='password'
+						placeholder='Password'
+						value={info.password || ''}
+						onChange={handleChange}
+					/>
+					<label htmlFor='confirmPassword'>Confirm Password</label>
+					<input
+						type='password'
+						id='confirm'
+						value={confirmPassword || ''}
+						onChange={handleChange}
+					/>
+					<input type='submit' />
+					<button type='button' onClick={handleCancel}>
+						Cancel
+					</button>
+					<p>Passwords must match.</p>
+				</form>
+			</Container>
 		);
 }
 
