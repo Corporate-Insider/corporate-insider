6212129b37750cf87b63b80f1cbc19af3bd9278b
alishalawani <alishalawani@gmail.com>
2020-09-09 22:38:18 -0500

    added modal for creating a new review

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,30 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import {Image, Card, Container} from 'react-bootstrap';
+import { Image, Card, Container } from 'react-bootstrap';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import Search from './Search';
 
-
-function Home({companies}) {
-  
-    let cards = companies.map((company, index)=>{
-        return (
-					<Card key={index} className='mainCard'>
-						<Card.Body className='card'>
-							<Link to={`insight/${company.name}`}>
-								<Image src={company.logo} className='companyLogo' />
-							</Link>
-							<Link to={`insight/${company.name}`} className='companyName'>
-								<h5>{company.name}</h5>
-							</Link>
-						</Card.Body>
-					</Card>
-				);
-    })
-    return (
-			<div>
-				{cards}
-			</div>
+function Home({ companies }) {
+	let cards = companies.map((company, index) => {
+		return (
+			<Card key={index} className='mainCard'>
+				<Link to={`insight/${company.name}`}>
+					<Image src={company.logo} className='companyLogo' />
+				</Link>
+				<Link to={`insight/${company.name}`} className='companyName'>
+					<h5>{company.name}</h5>
+				</Link>
+			</Card>
 		);
+	});
+	return (
+		<div>
+			<Search  companies={companies}/>
+			<Container className='homeContainer'>{cards}</Container>
+		</div>
+	);
 }
 
 export default Home;
