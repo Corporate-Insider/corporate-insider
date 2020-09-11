635730b5b88c429e925c3e1527d95d8131d237ff
alishalawani <alishalawani@gmail.com>
2020-09-10 19:58:13 -0500

    made modifications in app.js

--- src/App.js
+++ src/App.js
@@ -3,38 +3,58 @@ import './App.css';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import { Container } from 'react-bootstrap';
-import {Route, Redirect} from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import Login from './Components/Login';
 import SignUp from './Components/SignUp';
 import About from './Components/About';
 import Insight from './Components/Insight';
 
-
 const url = `http://localhost:8000/companies/`;
+const currentUserURL = 'http://localhost:8000/accounts/current_user/';
 function App() {
-  let [loggedIn, setLoggedIn] = useState(
+	let [loggedIn, setLoggedIn] = useState(
 		localStorage.getItem('token') ? true : false
-  );
-  
-  let [username, setUsername] = useState(localStorage.getItem('user'))
+	);
 
-  let [companies, setCompanies] = useState([]);
-  const fetchCompanies = ()=>{
-    fetch(url)
+	let [username, setUsername] = useState('');
+
+	let [companies, setCompanies] = useState([]);
+	const fetchCompanies = () => {
+		fetch(url)
 			.then((res) => {
 				return res.json();
 			})
 			.then((res) => {
 				setCompanies(res);
 			});
-  }
+	};
 
 	useEffect(() => {
 		fetchCompanies();
+		if (loggedIn && localStorage.getItem('token')) {
+			fetch(currentUserURL, {
+				headers: {
+					Authorization: `JWT ${localStorage.getItem('token')}`,
+				},
+			})
+				.then((res) => res.json())
+				.then((res) => {
+          console.log(res);
+					if (res.name) {
+						setUsername(res.name);
+					} else {
+						setLoggedIn(false);
+					}
+				})
+				.catch((error, status) => {
+					if (status === '401') {
+						setLoggedIn(false);
+					}
+				});
+		}
 	}, []);
 
-
-  return (
+	return (
 		<Container className='App'>
 			<Header
 				loggedIn={loggedIn}
@@ -47,7 +67,7 @@ function App() {
 				path='/'
 				exact
 				render={() => {
-					return <Home companies={companies} />;
+					return <Home companies={companies} fetchCompanies ={fetchCompanies}/>;
 				}}
 			/>
 			<Route path='/about' component={About} />
@@ -68,6 +88,7 @@ function App() {
 				render={(routerProps) => {
 					return (
 						<Insight
+							loggedIn={loggedIn}
 							match={routerProps.match}
 							companies={companies}
 							fetchCompanies={fetchCompanies}
