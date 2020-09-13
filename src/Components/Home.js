555cb92f1d187c476c2b2b2956f03a190b6d2f80
alishalawani <alishalawani@gmail.com>
2020-09-13 04:54:19 -0500

    fixed homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,23 +3,56 @@ import { Image, Card, Container } from 'react-bootstrap';
 import './Home.css';
 import { Link } from 'react-router-dom';
 import Search from './Search';
+import StaticRating from './StaticRating';
+function Home({ companies, fetchCompanies, loggedIn }) {
 
-function Home({ companies, fetchCompanies }) {
 	let cards = companies.map((company, index) => {
+        	let defaultRating;
+					let five = 0;
+					let four = 0;
+					let three = 0;
+					let two = 0;
+					let one = 0;
+					let rated = false;
+					if (company) {
+						company.ratings.forEach((rating) => {
+							if (rating.rating === 5) {
+								five += 1;
+							} else if (rating.rating === 4) {
+								four += 1;
+							} else if (rating.rating === 3) {
+								three += 1;
+							} else if (rating.rating === 2) {
+								two += 1;
+							} else if (rating.rating === 1) {
+								one += 1;
+							}
+						});
+					}
+					console.log(company);
+					if (company) {
+						defaultRating =
+							(5 * five + 4 * four + 3 * three + 2 * two + one) /
+							(five + four + three + two + one);
+					}
+
 		return (
 			<Card key={index} className='mainCard'>
 				<Link to={`insight/${company.name}`}>
 					<Image src={company.logo} className='companyLogo' />
 				</Link>
-				<Link to={`insight/${company.name}`} className='companyName'>
-					<h5>{company.name}</h5>
-				</Link>
+				<Container className='homeItems'>
+					<Link to={`insight/${company.name}`} className='companyName'>
+						<h5 >{company.name}</h5>
+					</Link>
+					<StaticRating defaultRating={defaultRating} />
+				</Container>
 			</Card>
 		);
 	});
 	return (
 		<div>
-			<Search companies={companies} fetchCompanies={fetchCompanies} />
+			<Search companies={companies} fetchCompanies={fetchCompanies} loggedIn={loggedIn}/>
 			<Container className='homeContainer'>{cards}</Container>
 		</div>
 	);
