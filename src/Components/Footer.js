4e03a7288cf782acc805508b33eef284f7e5df1a
alishalawani <alishalawani@gmail.com>
2020-09-13 04:48:16 -0500

    added footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
+function Footer(props) {
+	return (
+		<footer className='footer'>
+			<div className='top-line'>
+				{' '}
+				<span>
+					&copy; 2020{' '}
+					<Link to='/' className='loGo'>
+						Corporate Insider{' '}
+					</Link>{' '}
+					By
+					<img
+						src='https://i.imgur.com/BO1FpTO.png'
+						alt='lisha-logo'
+						className='lisha-logo'
+					/>
+				</span>
+			</div>
+			<div>
+				<span>Powered by </span>
+				<Link to='/' className='loGo'>
+					Corporate Insider
+				</Link>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
