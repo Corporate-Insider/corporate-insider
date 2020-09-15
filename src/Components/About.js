22ef35e05dc5a6694232cf663ecaa631f58f02f4
alishalawani <alishalawani@gmail.com>
2020-09-14 22:11:42 -0500

    updated the about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,13 +3,21 @@ import {Jumbotron} from 'react-bootstrap';
 import './About.css';
 function About(props) {
     return (
-			<Jumbotron>
-				<h1 className='about-heading'>About Corporate Insider</h1>
+			<Jumbotron className='jumbo'>
+				<h2 className='about-heading'>About Corporate Insider</h2>
 				<p className='about-paragraph'>
 					Corporate Insider was created for the sole purpose of helping job
-					searchers get insights about their prospective employers from other
-					members of the Corporate Insider community that have worked or
-					currently work at the respective companies
+					searchers get insights about their prospective employers, from other
+					members of the Corporate Insider community who have worked or
+					currently work at the respective companies.
+				</p>
+                <br/>
+				<hr className='hr' />
+
+				<p className='about-paragraph'>
+					We would really appreciate your reviews of the companies you have
+					worked with as it would be of great help to other members of the
+					Corporate Insider community who are actively job searching.
 				</p>
 			</Jumbotron>
 		);
